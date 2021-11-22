eb46b11f69f8d3c5211be2f83c7dc07deff4aa4c
Matthew Cengia <mattcen@mattcen.com>
2021-11-22 16:21:24 +1100

    Alter display of Django Admin interface

--- RadioActiv8/admin.py
+++ RadioActiv8/admin.py
@@ -3,6 +3,9 @@ from .models import *
 
 class EventAdmin(admin.ModelAdmin):
     list_display= ('timestamp', 'patrol', 'location', 'intelligence_request', 'intelligence_answered_correctly', 'destination', 'comment')
+    list_editable= ('patrol', 'location', 'intelligence_request', 'intelligence_answered_correctly', 'destination', 'comment')
+    list_filter= ('patrol', 'location', 'destination')
+    search_fields= ('patrol__name', 'location__radio__location_name', 'intelligence_request__answer', 'destination__radio__location_name', 'comment')
 
 # Register your models here.
 admin.site.register(Patrol, admin.OSMGeoAdmin)
@@ -12,3 +15,5 @@ admin.site.register(Base, admin.OSMGeoAdmin)
 admin.site.register(Intelligence)
 admin.site.register(Queue)
 admin.site.register(Event, EventAdmin)
+
+admin.site.site_header = "RadioActiv8 Admin"
