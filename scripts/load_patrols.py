f8e9223df772dbd6ee90aeb067d38869c2529bf8
Matthew Cengia <mattcen@mattcen.com>
2021-12-10 11:29:08 +1100

    Add helper scripts

---
+++ scripts/load_patrols.py
@@ -0,0 +1,9 @@
+# docker compose exec app ./manage.py shell -c 'import load_patrols'
+from RadioActiv8.models import *
+
+with open('patrols.txt', 'r') as f:
+    lines = f.readlines()
+
+for line in lines:
+    p = Patrol(name=line.strip())
+    p.save()
