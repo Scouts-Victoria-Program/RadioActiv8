15e3b88032e52e6ab5aa880c44e4e48e4cb916da
Matthew Cengia <mattcen@mattcen.com>
2021-12-29 23:10:53 +1100

    Remove leading RadioActiv8/ from URL

--- RadioActiv8/geodjango/urls.py
+++ RadioActiv8/geodjango/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('RadioActiv8/', include('RadioActiv8.urls')),
+    path('', include('RadioActiv8.urls')),
 ]
