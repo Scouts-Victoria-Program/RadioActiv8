8d46a9bea0f2b48a3b0f56197a5e7de9622ffb04
Matthew Cengia <mattcen@mattcen.com>
2022-06-18 14:25:16 +1000

    Add history field to all models

--- django/RadioActiv8/admin.py
+++ django/RadioActiv8/admin.py
@@ -12,6 +12,7 @@ from .models import (
 )
 
 from RadioActiv8.forms import EventForm
+from simple_history.admin import SimpleHistoryAdmin
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 import csv
@@ -90,7 +91,7 @@ class SessionAdmin(SimpleHistoryAdmin):
     pass
 
 
-class GPSTrackerAdmin(SimpleHistoryAdmin):
+class GPSTrackerAdmin(admin.ModelAdmin):
     pass
 
 
