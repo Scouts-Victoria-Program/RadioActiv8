b5725dd20f88fcb2767d8c61b8f11c28629098ec
Matthew Cengia <mattcen@mattcen.com>
2022-06-30 09:23:16 +1000

    STS: Add member_class and project_patrol fields to Patrol model

--- django/RadioActiv8/models.py
+++ django/RadioActiv8/models.py
@@ -7,6 +7,7 @@ import random
 from django.utils import timezone
 from datetime import timedelta
 from simple_history.models import HistoricalRecords
+from scoutsvic_extranet.models import MemberClass
 
 # FIXME: This default should be configurable
 DEFAULT_POINT = Point(144.63760, -36.49197)
@@ -182,6 +183,8 @@ class Patrol(models.Model):
     preferred_bases = models.ManyToManyField(
         Base, blank=True, related_name="patrol_preferred"
     )
+    member_classes = models.ManyToManyField(MemberClass, blank=True)
+    project_patrol = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["name"]
