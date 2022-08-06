483c15ef7a8bd61f1169accd6356ca28137f2e50
Matthew Cengia <mattcen@mattcen.com>
2022-08-06 13:22:02 +1000

    Move static URL stuff to project instead of app

--- RadioActiv8/geodjango/urls.py
+++ RadioActiv8/geodjango/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib.gis import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('RadioActiv8.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
