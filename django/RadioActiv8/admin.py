072fa9d375be202484b723296d03b40375a5e355
Matthew Cengia <mattcen@mattcen.com>
2022-09-18 20:42:29 +1000

    Add csv export feature to admin interface

--- django/RadioActiv8/admin.py
+++ django/RadioActiv8/admin.py
@@ -1,6 +1,30 @@
 from django.contrib.gis import admin
 from .models import *
 from RadioActiv8.forms import EventForm
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+import csv
+
+
+@admin.action(description='Download selected as csv')
+def download_csv(modeladmin, request, queryset):
+    if not request.user.is_staff:
+        raise PermissionDenied
+    opts = queryset.model._meta
+    model = queryset.model
+    response = HttpResponse(content_type='text/csv')
+    # force download.
+    response['Content-Disposition'] = 'attachment;filename=export.csv'
+    # the csv writer
+    writer = csv.writer(response)
+    field_names = [field.name for field in opts.fields]
+    # Write a first row with header information
+    writer.writerow(field_names)
+    # Write data rows
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in field_names])
+    return response
+admin.site.add_action(download_csv, 'download_csv')
 
 class EventAdmin(admin.ModelAdmin):
     list_display= ('timestamp', 'session', 'patrol', 'location', 'intelligence_request', 'intelligence_answered_correctly', 'destination', 'comment')
