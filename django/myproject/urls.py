6fe21875edd260214b0a38fcbf20637430b1f156
Matthew Cengia <mattcen@mattcen.com>
2023-01-02 18:01:30 +1100

    Rename dirs to match upstream Django template

--- django/myproject/urls.py
+++ django/myproject/urls.py
@@ -1,4 +1,4 @@
-"""geodjango URL Configuration
+"""myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
