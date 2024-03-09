2021aaa0a14b0b3ed4e7f7710984ca12b09713ee
Matthew Cengia <mattcen@mattcen.com>
2024-03-09 19:15:15 +1100

    Merge migrations

--- django/RadioActiv8/migrations/0023_patrol_preferred_bases.py
+++ django/RadioActiv8/migrations/0023_patrol_preferred_bases.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('RadioActiv8', '0022_historicalsession_historicalradio_historicalpatrol_and_more'),
+        ('RadioActiv8', '0021_alter_location_session'),
     ]
 
     operations = [
